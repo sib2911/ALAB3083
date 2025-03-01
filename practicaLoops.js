6edf5ccb09e9976b01acc5671e7d1a0920bfffd8
Sebastian Garcia <198815668+sib2911@users.noreply.github.com>
2025-02-28 21:43:09 -0800

    part 1
    part 1 complete

--- practicaLoops.js
+++ practicaLoops.js
@@ -1,1 +1,37 @@
 //Part 1
+// To begin, solve the following classic “Fizz Buzz” problem.
+// There are a few different ways to do this - experiment with what you think is the most efficient.
+// Once you have solved the problem, ask yourself if there could be another way; and if so, would it be better.
+// Accomplish the following:
+// Loop through all numbers from 1 to 100.
+// If a number is divisible by 3, log “Fizz.”
+// If a number is divisible by 5, log “Buzz.”
+// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+// If a number is not divisible by either 3 or 5, log the number.
+// Remember to commit your solution once it is working.
+
+// const str = "Hello World";
+
+// for (let i = 0; i < str.length; i++) {
+// 	if (str[i] == "l") {
+// 		continue;
+// 	}
+
+// 	console.log(str[i]);
+// }
+
+for (i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("Fuzz Buzz");
+        continue;
+      }
+  if (i % 3 == 0) {
+    console.log("fuzz");
+    continue;
+  }
+  if (i % 5 == 0) {
+    console.log("buzz");
+    continue;
+  }
+  else console.log(i)
+}
